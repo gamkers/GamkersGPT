3951f774bcdc9a8b505216c79964afd612ccf663
AKASH <87299311+gamkers@users.noreply.github.com>
2025-06-01 14:53:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -622,23 +622,23 @@ class CyberSecurityAssistant:
     def __init__(self, groq_api_key):
 
         
-        self.llm = ChatXAI(
-            model="grok-beta",
-            temperature=0.3,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2,
-            xai_api_key=st.secrets["xapi"],
-        )
-        
-        # self.llm = ChatGroq(
-        #     model="llama-3.3-70b-versatile",
+        # self.llm = ChatXAI(
+        #     model="grok-beta",
         #     temperature=0.3,
         #     max_tokens=None,
         #     timeout=None,
         #     max_retries=2,
-        #     groq_api_key=groq_api_key,
+        #     xai_api_key=st.secrets["xapi"],
         # )
+        
+        self.llm = ChatGroq(
+            model="llama-3.3-70b-versatile",
+            temperature=0.3,
+            max_tokens=None,
+            timeout=None,
+            max_retries=2,
+            groq_api_key=groq_api_key,
+        )
         
         self.prompt = ChatPromptTemplate.from_messages([
             (
